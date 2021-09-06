f4b0bc33528f2974691146654c9abcde42f15f7b
croschel <croschel000@gmail.com>
2021-09-05 23:16:47 -0300

    rn elements configured

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, StatusBar, StyleSheet, SafeAreaView, Text} from 'react-native';
+import {Card, Button} from 'react-native-elements';
 
 import {Deck} from './src/components/Deck';
 import {CARDS} from './src/mocks/cards';
 
 const App = () => {
   const renderCard = (item: ItemCard) => {
-    return <Text>{item.text}</Text>;
-    /* return (
+    // return <Text>{item.text}</Text>;
+    return (
       <Card key={item.id}>
         <Card.Title>{item.text}</Card.Title>
         <Card.Image source={{uri: item.uri}} />
-        <Text>I can customize this further.</Text>
+        <Text style={{marginVertical: 16}}>I can customize this further.</Text>
         <Button
           icon={{name: 'code'}}
           title="View now!"
           buttonStyle={{backgroundColor: '#03A9F4'}}
         />
       </Card>
-    ); */
+    );
   };
   return (
     <SafeAreaView>
