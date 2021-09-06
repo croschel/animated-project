1033919734cd1c1ec8b4ac7a523f7c6f736c2528
croschel <croschel000@gmail.com>
2021-09-06 14:38:01 -0300

    correct rotate card

--- src/components/Deck/index.tsx
+++ src/components/Deck/index.tsx
@@ -1,5 +1,6 @@
 import React, {ReactNode, useRef} from 'react';
 import {View, PanResponder, Animated} from 'react-native';
+import {SCREEN_WIDTH} from '../../utils/metrics';
 
 import {styles} from './styles';
 
@@ -14,24 +15,44 @@ export const Deck = ({data, renderCard}: DeckProps) => {
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        const {dx, dy} = gesture;
-        position.setValue({x: dx, y: dy});
+        const {dx} = gesture;
+        position.setValue({x: dx, y: 0});
       },
       onPanResponderRelease: () => {},
     }),
   ).current;
 
+  const getCardStyles = () => {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg'],
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [
+        {
+          rotate,
+        },
+      ],
+    };
+  };
+
   const renderAllCards = () => {
-    return data.map(card => {
+    return data.map((card, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={card.id}
+            {...panResponder.panHandlers}
+            style={[styles.container, getCardStyles()]}>
+            {renderCard(card)}
+          </Animated.View>
+        );
+      }
       return renderCard(card);
     });
   };
 
-  return (
-    <Animated.View
-      {...panResponder.panHandlers}
-      style={[styles.container, position.getLayout()]}>
-      {renderAllCards()}
-    </Animated.View>
-  );
+  return <View style={styles.container}>{renderAllCards()}</View>;
 };
